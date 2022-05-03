2bd1696d833aa3618f701c141a9f08c9b2a06834
신지우 <ziu974@gmail.com>
2022-05-03 16:18:22 +0900

    [S06P31A103-99] :sparkles: Feat: Modal common component

--- Frontend/app/theme/Color/Color.ts
+++ Frontend/app/theme/Color/Color.ts
@@ -12,4 +12,16 @@ const color: colorType = {
   textColor,
 };
 
+/**
+ * 신지우 - 임시
+ */
+export const colors = {
+  PRIMARY: '#ffc048',
+
+  BLACK: '#1e272e',
+  DARK_GREY: '#d2d2d2',
+  LIGHT_GREY: '#808e9b',
+  WHITE: '#ffffff',
+};
+
 export default color;
