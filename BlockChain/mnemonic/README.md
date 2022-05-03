c3af1f68742593b3339f10e579f33391c93b1057
park <horusr8@gmail.com>
2022-05-03 16:33:32 +0900

    [S06P31A103-188 ✨ Feat: Add tadditional wallet address

--- BlockChain/mnemonic/README.md
+++ BlockChain/mnemonic/README.md
@@ -9,7 +9,7 @@ npm install ethereumjs-tx
 
 ```javascript
 createMnemoic : 니모닉키(영어 12구문)을 생성합니다.
-getSolanaKey : 니모닉키로 솔라나 지갑주소를 가져옵니다.
+getSolanaKey : 니모닉키로 솔라나 지갑주소를 가져옵니다. 
 getWalletKey : 니모닉키로 이더리움 지갑주소를 가져옵니다.
 getEtherBalance : 이더리움 네트워크 기반의 잔고를 조회합니다.
 getSolanaBalance: 솔라나 네트워크 기반의 잔고를 조회합니다.
--- BlockChain/mnemonic/getSolanaKey.js
+++ BlockChain/mnemonic/getSolanaKey.js
@@ -8,10 +8,14 @@ const solanaWeb3 = require('@solana/web3.js')
 // temp_key
 const mnemonicKey = 'pottery mistake dry purchase limit spawn consider steak wagon fashion feed edit student work lake you boss lecture march remember toddler drop drop frog'
 
+
+// 지갑 생성 기본값 0
+let walletNumber = 0
+
 // 니모닉키를 이용해서 지갑을 생성합니다.
 const getSolanaKey = async() =>{
     const seed = await bip39.mnemonicToSeed(mnemonicKey)
-    const derivePath = "m/44'/501'/0'/0'";
+    const derivePath = `m/44'/501'/0'/${walletNumber}'`;
     const derivedSeed = ed25519.derivePath(derivePath,seed.toString('hex')).key
     const solanaKey = solanaWeb3.Keypair.fromSeed(derivedSeed);
     console.log(solanaKey)
