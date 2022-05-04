352481fa72a858d0006d2b98816aac4346a1d443
YoonBaek <royal729@naver.com>
2022-05-04 09:13:15 +0900

    [S06P31A103-59] ✨ Feat: 니모닉 카드 추가

--- Frontend/app/screens/Wallet/Wallet.tsx
+++ Frontend/app/screens/Wallet/Wallet.tsx
@@ -4,6 +4,7 @@ import { ScrollView } from 'react-native';
 import React from 'react';
 import SecurityCard from '@containers/SecurityCard';
 const Wallet = () => {
+  // SecurityCard 임시
   return (
     <ScrollView>
       <SecurityCard />
