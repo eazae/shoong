3b1b5392f2b4e4182d95aaa9e8b994b2f3b2f0c1
신지우 <ziu974@gmail.com>
2022-05-04 18:19:05 +0900

    [S06P31A103-117] :sparkles: Feat: axios, eth, sol instance

---
+++ Frontend/app/services/api/axios.ts
@@ -0,0 +1,71 @@
+import axios from 'axios';
+import { Alert } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
+import { getSecureStoreValue, setSecureStoreValue } from '@utils/secureStore';
+import { API_BASE_URL } from '@env';
+
+const instance = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 30000,
+  headers: {
+    'Content-type': 'application/json',
+    'Access-Control-Allow-Credentials': true,
+  },
+});
+
+/* Apply Interceptor */
+// HTTP request interceptor
+instance.interceptors.request.use(
+  (config) => {
+    const jwt = getSecureStoreValue('jwt');
+    if (jwt) {
+      config.headers!.Authorization = 'Bearer ' + jwt;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+// HTTP response interceptor
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  function (error) {
+    if (error.response) {
+      console.log(error.response);
+      switch (error.response.status) {
+        /* 'JWT expired' exeption */
+        case 400:
+          console.log('400 ERROR, not authorized.');
+          break;
+        case 401:
+          console.log('401error!');
+          Alert.alert('문제가 발생했습니다. 잠시 후 시도해주세요.');
+          break;
+        case 404:
+          console.log('404error!');
+          break;
+        case 409:
+          console.log('409error!');
+          break;
+        default:
+      }
+    } else {
+      // ex. 서버 키지 않은 경우
+    }
+    return Promise.reject(error);
+  }
+);
+
+export const multipartInstance = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 30000,
+  headers: {
+    'Content-Type': `multipart/form-data`,
+  },
+});
+
+export default instance;
