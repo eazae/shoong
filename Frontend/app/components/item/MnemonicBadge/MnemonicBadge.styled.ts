0805d978af96615b67b08b53cdba86d79bb393d4
YoonBaek <royal729@naver.com>
2022-05-04 09:06:00 +0900

    [S06P31A103-59] ✨ Feat: 니모닉 뱃지 추가

---
+++ Frontend/app/components/item/MnemonicBadge/MnemonicBadge.styled.ts
@@ -0,0 +1,15 @@
+import Color from '@theme/Color';
+import styled from 'styled-components/native';
+import { IMnemonicBadge } from './MnemonicBadge.props';
+
+export const LayOut = styled.View<IMnemonicBadge>`
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  border-radius: 12px;
+  padding: 8px 5px;
+  margin: 0px 5px;
+  border: 1.5px ${({ variant }) => variant} ${Color.borderColor.primary};
+`;
