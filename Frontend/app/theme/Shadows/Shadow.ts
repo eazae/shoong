0a9cc67607238b0d373e5959bef5cdb86ca56c5e
YoonBaek <royal729@naver.com>
2022-05-04 15:43:48 +0900

    [S06P31A103-98] ✨ Feat: button color 추가

--- Frontend/app/theme/Shadows/Shadow.ts
+++ Frontend/app/theme/Shadows/Shadow.ts
@@ -1,6 +1,6 @@
 const Shadow = {
-  common: '0px 1px 3px rgba(0,0,0,0.1)',
-  button: '0px 1px 1px rgba(0,0,0,0.05)',
+  common: '0px 1px 3px rgba(0,0,0,0.15)',
+  button: '0px 1px 1px rgba(0,0,0,0.1)',
 };
 
 export default Shadow;
