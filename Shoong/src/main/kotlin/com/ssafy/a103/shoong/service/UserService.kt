739db543575a8c46c034d88f7695db3ad49c9390
DESKTOP-1GKQ6C0\sejin <qkrtpwls1994@gmail.com>
2022-05-06 17:30:27 +0900

    :sparkles: Login API 구현

--- Shoong/src/main/kotlin/com/ssafy/a103/shoong/service/UserService.kt
+++ Shoong/src/main/kotlin/com/ssafy/a103/shoong/service/UserService.kt
@@ -4,13 +4,21 @@ import com.ssafy.a103.shoong.model.User
 import com.ssafy.a103.shoong.repository.UserRepository
 import com.ssafy.a103.shoong.repository.UserRepositorySupport
 import com.ssafy.a103.shoong.requestBody.UserJoinRequestBody
+import com.ssafy.a103.shoong.requestBody.UserLoginRequestBody
+import io.jsonwebtoken.Jwt
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.stereotype.Service
+import java.util.*
+import javax.servlet.http.Cookie
 
 @Service
 class UserService(val userRepository: UserRepository, val userRepositorySupport: UserRepositorySupport) {
     fun getAll(): List<User> = userRepository.findAll()
 
     fun getByEmail(email: String): User = userRepositorySupport.getByEmail(email).orElse(null)
+
+    fun getById(id:String):User = userRepositorySupport.getById(id).orElse(null)
 
     fun join(userJoinRequestBody: UserJoinRequestBody): User {
         val user = User()
@@ -20,4 +28,5 @@ class UserService(val userRepository: UserRepository, val userRepositorySupport:
         user.user_nickname = userJoinRequestBody.user_nickname
         return userRepository.save(user)
     }
+
 }
