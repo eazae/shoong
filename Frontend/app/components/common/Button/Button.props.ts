911b26e6dbacaade483485614a0256e48257bc1b
YoonBaek <royal729@naver.com>
2022-05-09 19:50:55 +0900

    [S06P31A103-98] ✨ Feat: Button Component 업데이트

--- Frontend/app/components/common/Button/Button.props.ts
+++ Frontend/app/components/common/Button/Button.props.ts
@@ -1,5 +1,4 @@
 import { buttonColorNames } from '@theme/Color/Color.props';
-import { IconProps } from 'phosphor-react-native';
 
 export interface ButtonProps {
   icon?: JSX.Element;
--- Frontend/app/components/common/Button/Button.tsx
+++ Frontend/app/components/common/Button/Button.tsx
@@ -1,13 +1,11 @@
-import Color from '@theme/Color';
 import Typography from '@theme/Typography';
-import { Wallet } from 'phosphor-react-native';
 import { ButtonProps } from './Button.props';
 import { ButtonContainer, IconLayOut } from './Button.styled';
 
 const Button: React.FC<ButtonProps> = ({ icon, onPress, title, variant = 'primary' }) => (
   <ButtonContainer onPress={onPress} variant={variant}>
     <IconLayOut>{icon}</IconLayOut>
-    <Typography weight="regular" color="light">
+    <Typography weight="regular" color={variant === 'transparent' ? 'primary' : 'light'}>
       {title}
     </Typography>
   </ButtonContainer>
