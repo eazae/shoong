0aa2ef3bf186e723d1725bf375bcd099052e8200
YoonBaek <royal729@naver.com>
2022-05-09 20:13:35 +0900

    [S06P31A103-57] ✨ Feat: Join 스크린 추가

---
+++ Frontend/app/screens/Join/JoinCreateOrLoad/JoinCreateOrLoad.tsx
@@ -0,0 +1,32 @@
+import Button from '@components/common/Button';
+import HeaderGoBackBtn from '@components/item/HeaderGoBackBtn';
+import { LayOut } from '@screens/Login/Login.styled';
+import Color from '@theme/Color';
+import { Cardholder } from 'phosphor-react-native';
+import styled from 'styled-components/native';
+import Instruction from '../Instruction';
+
+const messages = ['계정정보 입력을', '성공적으로 마쳤어요', '연동할 지갑이 있으신가요?'];
+
+const JoinCreateOrLoad = ({ navigation: { navigate } }) => {
+  return (
+    <>
+      <HeaderGoBackBtn />
+      <LayOut>
+        <Instruction messages={messages} />
+        <Seperator />
+        <Button variant="transparent" title="네, 제 지갑을 연동할게요" />
+        <Button
+          onPress={() => navigate('Join', { screen: 'JoinMnemonicIntroduce' })}
+          icon={<Cardholder color={Color.textColor.light} />}
+          title="지갑을 새로 생성하고 싶어요"
+        />
+      </LayOut>
+    </>
+  );
+};
+
+const Seperator = styled.View`
+  height: 50px;
+`;
+export default JoinCreateOrLoad;
