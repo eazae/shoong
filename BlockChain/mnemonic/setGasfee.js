bafd96d1653e6baff36ad3ef3aa9cc3d3647c178
park <horusr8@gmail.com>
2022-05-10 16:31:53 +0900

    [S06P31A103-125] :spakles: Feat: add eth unit set

--- BlockChain/mnemonic/setGasfee.js
+++ BlockChain/mnemonic/setGasfee.js
@@ -27,9 +27,15 @@ const sendTime = async (gas) =>{
     return second
 }
 
-// 입력된 값을 이더단위로 바꾸기
+// 입력된 이더값 wei단위로 바꾸기
 const getEth = (num) =>{
     const inputEth = Web3.utils.toWei(`${num}`, 'ether')
     const convertEth = Web3.utils.toHex(inputEth)
     return convertEth
+}
+
+// 입력된 토큰값 단위 wei로 바꾸기
+const getToken = (num) =>{
+    const inputToken = Web3.utils.toWei(`${num}`, 'ether')
+    return inputToken
 }
