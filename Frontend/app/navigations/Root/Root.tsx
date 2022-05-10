24a2fe4cdd3b2f28056753ce0090bb01dead8e75
eazae <xynezae@gmail.com>
2022-05-10 13:49:21 +0900

    [S06P31A103-55] :sparkles: Feat: User loggedIn state using recoil, add logout function

--- Frontend/app/navigations/Root/Root.tsx
+++ Frontend/app/navigations/Root/Root.tsx
@@ -2,13 +2,34 @@ import JoinNav from '@navigations/JoinNav';
 import Tab from '@navigations/Tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '@screens/Login';
-import { useState } from 'react';
+import { clearJWTValue, getJWTValue, setJWTValue } from '@utils/secureStore';
+import { isLoggedState } from '@atoms/atoms';
+import { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { Alert } from 'react-native';
 
 const Nav = createNativeStackNavigator();
 
 const Root = () => {
   // test
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const isLogged = useRecoilValue(isLoggedState);
+
+  const checkLoggedIn = async () => {
+    const data = await getJWTValue();
+    console.log('JWT:', data);
+    setIsLoggedIn(data !== null);
+  };
+
+  useEffect(() => {
+    // 로그인 시 JWT 저장
+    // setJWTValue('테스트');
+
+    // 로그아웃 시 JWT 삭제
+    // clearJWTValue();
+    checkLoggedIn();
+  }, [isLogged]);
+
   return (
     <Nav.Navigator
       screenOptions={{ presentation: 'modal', headerShown: false }}
