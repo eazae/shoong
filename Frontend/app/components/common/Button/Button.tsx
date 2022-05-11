333fb68bea47e34a9e576e28db0d964a0fc5714a
YoonBaek <royal729@naver.com>
2022-05-11 22:23:23 +0900

    [S06P31A103-98] ✨ Feat: Button Disabled 추가

--- Frontend/app/components/common/Button/Button.tsx
+++ Frontend/app/components/common/Button/Button.tsx
@@ -2,8 +2,8 @@ import Typography from '@theme/Typography';
 import { ButtonProps } from './Button.props';
 import { ButtonContainer, IconLayOut } from './Button.styled';
 
-const Button: React.FC<ButtonProps> = ({ icon, onPress, title, variant = 'primary' }) => (
-  <ButtonContainer onPress={onPress} variant={variant}>
+const Button: React.FC<ButtonProps> = ({ disabled, icon, onPress, title, variant = 'primary' }) => (
+  <ButtonContainer disabled={disabled} onPress={onPress} variant={variant}>
     <IconLayOut>{icon}</IconLayOut>
     <Typography weight="regular" color={variant === 'transparent' ? 'primary' : 'light'}>
       {title}
