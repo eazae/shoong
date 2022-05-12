ddbdb1c958d8c132882f2d29abb69c1853902017
park <horusr8@gmail.com>
2022-05-12 17:44:59 +0900

    :hammer: Refactor: 지갑, 송금, 조회 바꿈

--- BlockChain/mnemonic/ethConvert.js
+++ BlockChain/mnemonic/ethConvert.js
@@ -1,3 +1,5 @@
+import Web3 from "web3";
+
 // 입력된 이더값 wei단위로 바꾸기
 const getEth = (num) =>{
     const inputEth = Web3.utils.toWei(`${num}`, 'ether')
@@ -6,7 +8,17 @@ const getEth = (num) =>{
 }
 
 // 입력된 토큰값 단위 wei로 바꾸기
-const getToken = (num) =>{
-    const inputToken = Web3.utils.toWei(`${num}`, 'ether')
+const getToken = (num,decimals) =>{
+    let inputToken
+    switch(decimals){
+        case 18:
+            inputToken = Web3.utils.toWei(`${num}`, 'ether')
+            break;
+        case 6:
+            inputToken = Web3.utils.toWei(`${num}`, 'Mwei')
+            break;
+    }
     return inputToken
 }
+
+export {getEth, getToken} 
