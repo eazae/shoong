343466cbd7318f7f58b043beeb99cbd5a9b3dc67
DESKTOP-1GKQ6C0\sejin <qkrtpwls1994@gmail.com>
2022-05-13 14:48:57 +0900

    [S06P31A103-82] :sparkles: Feat : 친구추가 API 개발, 유저 API 수정

--- Shoong/src/main/kotlin/com/ssafy/a103/shoong/controller/UserController.kt
+++ Shoong/src/main/kotlin/com/ssafy/a103/shoong/controller/UserController.kt
@@ -1,10 +1,7 @@
 package com.ssafy.a103.shoong.controller
 
-import com.fasterxml.classmate.members.ResolvedParameterizedMember
 import com.ssafy.a103.shoong.model.User
-import com.ssafy.a103.shoong.requestBody.Message
-import com.ssafy.a103.shoong.requestBody.UserJoinRequestBody
-import com.ssafy.a103.shoong.requestBody.UserLoginRequestBody
+import com.ssafy.a103.shoong.requestBody.*
 import com.ssafy.a103.shoong.service.UserService
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
@@ -50,7 +47,7 @@ private class UserController(val userService: UserService) {
     @GetMapping("/api/user/getByEmail")
     fun getByEmail(@RequestParam email: String): ResponseEntity<User> {
         println("/api/user/getByEmail")
-        return ResponseEntity.ok().body(userService.getByEmail(email))
+        return ResponseEntity.ok().body(userService.getByEmail(email).get())
     }
 
     @Operation(summary = "join new user", description = "join new user")
@@ -65,23 +62,75 @@ private class UserController(val userService: UserService) {
         println("/api/user/join")
         return ResponseEntity.ok().body(userService.join(userJoinRequestBody))
     }
+
+    @PostMapping("api/user/checkemail")
+    @ApiResponses(value=[
+        ApiResponse(responseCode = "200", description = "OK !!"),
+        ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+        ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+        ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    ])
+    fun checkemail(@RequestBody userJoinRequestBody: UserJoinRequestBody): ResponseEntity<Any>{
+        if(userJoinRequestBody.user_email !=""){
+            val user = this.userService.getByEmail(userJoinRequestBody.user_email)
+            if(user != Optional.empty<User>() ){
+                return ResponseEntity.status(409).body(Message("Conflick"))
+            }
+        }
+        return ResponseEntity.ok(Message("success"))
+    }
+
+    @PostMapping("api/user/checknickname")
+    @ApiResponses(value=[
+        ApiResponse(responseCode = "200", description = "OK !!"),
+        ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+        ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+        ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    ])
+    fun checknickname(@RequestBody userJoinRequestBody: UserJoinRequestBody): ResponseEntity<Any>{
+        if(userJoinRequestBody.user_nickname !=""){
+            val user = this.userService.getByNickName(userJoinRequestBody.user_nickname)
+            if(user != Optional.empty<User>()){
+                return ResponseEntity.status(409).body(Message("Conflick"))//중복에러코드
+            }
+        }
+        return ResponseEntity.ok(Message("success"))
+    }
+    @PutMapping("api/user/update")
+    fun update(@CookieValue("jwt")jwt:String?,@RequestBody userUpdateRequestBody: UserUpdateRequestBody): ResponseEntity<Any>{
+        if(!this.userService.checkJWT(jwt)){
+            return ResponseEntity.status(401).body(Message("unauthenticated"))
+        }
+        val body = Jwts.parser().setSigningKey("secret").parseClaimsJws(jwt).body
+        val user = this.userService.getById(body.issuer.toString()).get()
+        println(userUpdateRequestBody)
+        return ResponseEntity.ok().body(this.userService.update(user,userUpdateRequestBody))
+    }
+    @PutMapping("api/user/updatepassword")
+    fun updatepassword(@CookieValue("jwt")jwt:String?,@RequestBody userUpdatePasswordRequestBody: UserUpdatePasswordRequestBody): ResponseEntity<Any>{
+        if(!this.userService.checkJWT(jwt)){
+            return ResponseEntity.status(401).body(Message("unauthenticated"))
+        }
+        val body = Jwts.parser().setSigningKey("secret").parseClaimsJws(jwt).body
+        val user = this.userService.getById(body.issuer.toString()).get()
+        return ResponseEntity.ok().body(this.userService.updatepassword(user,userUpdatePasswordRequestBody))
+    }
     @PostMapping("/api/user/login")
     fun login(@RequestBody userLoginRequestBody: UserLoginRequestBody, response:HttpServletResponse) : ResponseEntity<Any>{
-        println("/api/user/login")
         val user = this.userService.getByEmail(userLoginRequestBody.user_email)
-                ?:return ResponseEntity.badRequest().body(Message("user not found"))
-        if(!user.comparePassword(userLoginRequestBody.user_password)){
+        if(user==Optional.empty<User>()){
+            return ResponseEntity.badRequest().body(Message("user not found"))
+        }
+        if(!user.get().comparePassword(userLoginRequestBody.user_password)){
             return ResponseEntity.badRequest().body(Message("invalid pasword!"))
         }
-
-        var issuer = user.id.toString()
-
+        val issuer = user.get().id.toString()
+        println(issuer)
         val jwt = Jwts.builder()
                 .setIssuer(issuer)
-                .setExpiration(Date(System.currentTimeMillis()+60*24*1000))//1day
+                .setExpiration(Date(System.currentTimeMillis()+30*60*1000))//30min
                 .signWith(SignatureAlgorithm.HS512,"secret").compact()
-
-        var cookie = Cookie("jwt",jwt)
+        val cookie = Cookie("jwt",jwt)
         cookie.isHttpOnly = true
 
         response.addCookie(cookie)
@@ -90,16 +139,28 @@ private class UserController(val userService: UserService) {
     @PostMapping("/api/user/logout")
     fun logout(response:HttpServletResponse) : ResponseEntity<Any>{
         println("/api/user/logout")
-        var cookie = Cookie("jwt", "")
-        cookie.maxAge = 0
-        response.addCookie(cookie)
-
-        return ResponseEntity.ok(Message("success"))
+        return ResponseEntity.ok().body(this.userService.logout(response))
+    }
+    @DeleteMapping("/api/user/signout")
+    @ApiResponses(value=[
+        ApiResponse(responseCode = "200", description = "OK !!"),
+        ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+        ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+        ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    ])
+    fun signout(response: HttpServletResponse, @CookieValue("jwt")jwt:String?):ResponseEntity<Any>{
+        if(!this.userService.checkJWT(jwt)){
+            return ResponseEntity.status(401).body(Message("unauthenticated"))
+        }
+        val body = Jwts.parser().setSigningKey("secret").parseClaimsJws(jwt).body
+        val user = this.userService.getById(body.issuer.toString()).get()
+        logout(response)
+        return ResponseEntity.ok().body(user.id?.let { userService.signout(it) })
     }
     @GetMapping("/api/user/getUser")
     fun getUser(@CookieValue("jwt")jwt:String?):ResponseEntity<Any>{
         try {
-            if(jwt==null){
+            if(!this.userService.checkJWT(jwt)){
                 return ResponseEntity.status(401).body(Message("unauthenticated"))
             }
             var body = Jwts.parser().setSigningKey("secret").parseClaimsJws(jwt).body
@@ -110,4 +171,59 @@ private class UserController(val userService: UserService) {
         }
 
     }
+    @GetMapping("api/user/JWT")
+    @ApiResponses(value=[
+        ApiResponse(responseCode = "200", description = "OK !!"),
+        ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+        ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+        ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    ])
+    fun jwt(@CookieValue("jwt")jwt:String?):ResponseEntity<Any>{
+        if(!this.userService.checkJWT(jwt)){
+            return ResponseEntity.status(401).body(Message("unauthenticated"))
+        }
+        return ResponseEntity.ok(Message("success"))
+    }
+    //송금 시 비밀번호 인증
+    @PostMapping("api/user/password")
+    @ApiResponses(value=[
+        ApiResponse(responseCode = "200", description = "OK !!"),
+        ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+        ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+        ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    ])
+    fun password(@CookieValue("jwt")jwt:String?,@RequestBody certificationRequestBody:CertificationRequestBody):ResponseEntity<Any>{
+        if(!this.userService.checkJWT(jwt)){
+            return ResponseEntity.status(401).body(Message("unauthenticated"))
+        }
+        val body = Jwts.parser().setSigningKey("secret").parseClaimsJws(jwt).body
+        val user = this.userService.getById(body.issuer.toString()).get()
+        var email = user.user_email
+
+        if(!user.comparePassword(certificationRequestBody.user_password)){
+            println("wrong password")
+            return ResponseEntity.badRequest().body(Message("invalid pasword!"))
+        }
+
+        return ResponseEntity.ok(Message("Ok"))
+    }
+
+    @PostMapping("api/user/makefriend")
+    @ApiResponses(value=[
+        ApiResponse(responseCode = "200", description = "OK !!"),
+        ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+        ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+        ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    ])
+    fun makefriend(@CookieValue("jwt")jwt:String?,@RequestBody makeFriendRequestBody: MakeFriendRequestBody):ResponseEntity<Any>{
+        if(!this.userService.checkJWT(jwt)){
+            return ResponseEntity.status(401).body(Message("unauthenticated"))
+        }
+        var body = Jwts.parser().setSigningKey("secret").parseClaimsJws(jwt).body
+        var user = this.userService.getById(body.issuer.toString()).get()
+
+        this.userService.makeFriend(user, makeFriendRequestBody);
+        return ResponseEntity.ok(Message("Ok"))
+    }
 }
+
