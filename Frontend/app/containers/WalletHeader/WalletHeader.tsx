3138e551389b1455dc2af425146fcaf6d405423e
YoonBaek <royal729@naver.com>
2022-05-16 15:06:43 +0900

    [S06P31A103-65] ✨ Feat: 지갑 화면 헤더 추가

---
+++ Frontend/app/containers/WalletHeader/WalletHeader.tsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components/native';
+import { BlurView } from 'expo-blur';
+import { StyleSheet, useColorScheme } from 'react-native';
+import Avatar from '@components/common/Avatar';
+import Typography from '@theme/Typography';
+
+export const WALLET_HEADER_HEIGHT = 80;
+
+interface WalletHeaderProps {
+  userName: string;
+  totalBalance: number;
+  profileUri: string;
+}
+
+const WalletHeaderData: WalletHeaderProps = {
+  userName: '김싸피',
+  totalBalance: 100000,
+  profileUri: 'https://blog.kakaocdn.net/dn/YmhBn/btrheysMts6/GhjC6XXXhWC30n7Fmcqok1/img.jpg',
+};
+
+const WalletHeader = () => {
+  const isDark = useColorScheme() === 'dark';
+  const { profileUri, userName, totalBalance } = WalletHeaderData;
+  return (
+    <LayOut>
+      <BlurView tint={isDark ? 'dark' : 'light'} intensity={30} style={StyleSheet.absoluteFill}>
+        <ContentLayOut>
+          <TextLayOut>
+            <Typography size="body2" weight="bold">
+              {userName}
+              <Typography size="body3">님 안녕하세요</Typography>
+            </Typography>
+            <Typography size="body1" weight="bold">
+              $ <Typography size="body1">{totalBalance}</Typography>
+            </Typography>
+          </TextLayOut>
+          <Avatar size="small" isLoading={false} uri={profileUri} hasAlarm={true} />
+        </ContentLayOut>
+      </BlurView>
+    </LayOut>
+  );
+};
+
+const LayOut = styled.View`
+  height: ${WALLET_HEADER_HEIGHT}px;
+  background-color: transparent;
+`;
+
+const ContentLayOut = styled.View`
+  height: ${WALLET_HEADER_HEIGHT}px;
+  padding: 10px 20px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const TextLayOut = styled.View``;
+
+export default WalletHeader;
