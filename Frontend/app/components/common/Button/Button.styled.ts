1117c2b35c856b5e2bb45bc42aed04c9a5683460
YoonBaek <royal729@naver.com>
2022-05-17 01:34:42 +0900

    [S06P31A103-57] ✨ Feat: 버튼 컴포넌트 롤백 및 이더 월렛 dependencies 추가

--- Frontend/app/components/common/Button/Button.styled.ts
+++ Frontend/app/components/common/Button/Button.styled.ts
@@ -4,16 +4,11 @@ import { Dimensions } from 'react-native';
 import styled from 'styled-components/native';
 import { ButtonProps } from './Button.props';
 
-const { width: SCREEN_WIDTH } = Dimensions.get('window');
-
 export const ButtonContainer = styled.TouchableOpacity<ButtonProps>`
-  /* width: 100%; */
-  /* width: ${SCREEN_WIDTH}; */
+  width: 100%;
   padding: 15px;
-  margin: 6px 16px;
+  margin-bottom: 6px;
   border-radius: 8px;
-  /* background-color: ${(props) =>
-    props.disabled ? props.theme.disabledColor : props.theme.enabledColor}; */
   background-color: ${({ variant }) => Color.buttonColor[variant!]};
   flex-direction: row;
   justify-content: center;
