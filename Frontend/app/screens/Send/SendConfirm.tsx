7e4901985cb8a0733b580ad3ba32276f9bde75fb
Gyum <ksh7669@naver.com>
2022-05-17 20:03:34 +0900

    🎨 Style: 송금 페이지 내용 추가 및 에러 나는 부분 우선 제외

--- Frontend/app/screens/Send/SendConfirm.tsx
+++ Frontend/app/screens/Send/SendConfirm.tsx
@@ -1,13 +1,16 @@
 import Button from "@components/common/Button"
 import TextButton from "@components/common/TextButton/TextButton"
+import { useState } from "react"
 import { Text, View } from "react-native"
 
 export const SendConfirm = () => {
+    const [cancle, setCancle] = useState(false);
     return (
         <View>
             <Text>송금 확인</Text>
-            <TextButton title="송금 요청을 취소하고 싶어요!" />
-            <Button title="확 인" onPress={() => { console.log('메인화면으로 넘기기?') }} />
+            <TextButton title="송금 요청을 취소하고 싶어요!" onPress={() => { setCancle(true) }} />
+            <Text style={{ color: 'red' }}>(요청 취소 시 별도의 수수료가 들 수 있어요!)</Text>
+            <Button title="확 인" onPress={() => { }} />
         </View>
     )
 }
