afc06d1fb9003338534cbeef99eaa49c5b8cf005
YoonBaek <royal729@naver.com>
2022-05-18 18:19:25 +0900

    [S06P31A103-66] ✨ Feat: 지갑 잔액 불러오는 web3 Api 추가

---
+++ Frontend/app/services/web3/getBalance.ts
@@ -0,0 +1,48 @@
+import Web3 from 'web3';
+import * as solanaWeb3 from '@solana/web3.js';
+
+const web3 = new Web3('https://goerli.infura.io/v3/43b62be4d86946688b1fb5b4bf4df6c9');
+
+export const contractAddr = {
+  mana: '0x0F5D2fB29fb7d3CFeE444a200298f468908cC942',
+  tether: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+};
+
+export const getEthBalance = async (address: string) => {
+  // ether 잔액 조회
+  const ethBalance = await web3.eth.getBalance(address);
+  const convertEth = await web3.utils.fromWei(ethBalance.toString(), 'ether');
+  return Number(convertEth);
+};
+
+export const getTokenBalance = async (address: string, contractAddr: string) => {
+  // 토큰 잔액 조회
+  let minABI = [
+    // balanceOf
+    {
+      constant: true,
+      inputs: [{ name: '_owner', type: 'address' }],
+      name: 'balanceOf',
+      outputs: [{ name: 'balance', type: 'uint256' }],
+      type: 'function',
+    },
+  ];
+
+  // 토큰 잔액조회
+  const contractInstance = new web3.eth.Contract(minABI, contractAddr);
+  const contractMethod = await contractInstance.methods.balanceOf(address).call();
+  let convertToken;
+
+  convertToken = web3.utils.fromWei(contractMethod.toString(), 'ether');
+
+  return Number(convertToken);
+};
+
+export const getSolanaBalance = async (address: string) => {
+  const solanaAddress = new solanaWeb3.PublicKey(address);
+  const solana = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('devnet'), 'confirmed');
+
+  const balance = await solana.getBalance(solanaAddress);
+  console.log(`잔액조회 ${balance / 1000000000}`);
+  return balance / 1000000000;
+};
