c510e20b1328b38868ca91c7b0ae1a8b26a9e901
Gyum <ksh7669@naver.com>
2022-05-18 18:44:51 +0900

    :sparkles: 송금 확인 버튼 누를 시 원래 화면 돌아가기 Navigate 추가 및 Send 일부 수정

--- Frontend/app/screens/Send/Send.tsx
+++ Frontend/app/screens/Send/Send.tsx
@@ -18,15 +18,15 @@ import TokenList from '@containers/Send/TokenList/TokenList';
 import Input from '@components/common/TextInput/TextInput';
 
 interface SendProps {
-  address: string;
+    address: string;
 }
 interface ExpandProp {
-  width: string;
-  height: string;
+    width: string;
+    height: string;
 }
 interface IType {
-  type: string;
-  value: string;
+    type: string;
+    value: string;
 }
 
 const Container = styled.View`
@@ -36,209 +36,210 @@ const Container = styled.View`
 const defaultAddress = '';
 
 const Send: React.FC<SendProps> = ({ address }) => {
-  const [card, setCard] = useState({
-    width: WIDTH,
-    height: HEIGHT,
-  });
-  const [target, setTarget] = useState({
-    width: FOLDED,
-    height: FOLDED,
-  });
-  const [tokenFlag, setTokenFlag] = useState({
-    width: FOLDED,
-    height: FOLDED,
-  });
-  const [amountFlag, setAmountFlag] = useState({
-    width: FOLDED,
-    height: FOLDED,
-  });
-  const [visible, setVisible] = useState(false);
-  const [nick, setNick] = useState('');
-  const [phone, setPhone] = useState('');
-  const addr = '' + address;
-  const [fromAddress, setFromAddress] = useState('');
-  useEffect(() => {
-    if (address) {
-      setFromAddress(address);
-      expnadSwitch(target);
-    }
-  }, []);
-  const [to, setTo] = useState('');
-  const [price, setPrice] = useState(0);
-  const [token, setToken] = useState('');
-  const [amount, setAmount] = useState('');
-  //토큰
-  //수량
-
-  // @신지우
-  // 현재 선택된 단계
-  const [focus, setFocus] = useState(1);
-  const [selectedToken, setSelectedToken] = useState('');
-
-  const expnadSwitch = (val: ExpandProp) => {
-    const list = [card, target, tokenFlag, amountFlag];
-    const setList = [setCard, setTarget, setTokenFlag, setAmountFlag];
-    for (let i = 0; i < list.length; i++) {
-      if (list[i] === val) {
-        setList[i]({
-          width: WIDTH,
-          height: HEIGHT,
-        });
-      } else
-        setList[i]({
-          width: FOLDED,
-          height: FOLDED,
-        });
-    }
-  };
-
-  const goConfirm = () => {
-    //트랜잭션 id 들고 페이지 넘기기
-    //트랜잭션 요청 후 트랜잭션 id 가지고 다음 페이지로 이동
-    //@ts-ignore
-    navigate('Send', { screen: 'SendConfirm' });
-  };
-  const getType = (props: IType) => {
-    if (!props) return;
-    if (props.type === '전화번호') {
-      setPhone(props.value);
-      setNick('');
-    } else if (props.type === '닉네임') {
-      setNick(props.value);
-      setPhone('');
-    }
-    setTo('');
-  };
-  const getTo = (to: string) => {
-    setTo(to);
-    console.log(to);
-  };
-  const { navigate } = useNavigation();
-  return (
-    <Container>
-      <ScrollView nestedScrollEnabled={true}>
-        {/* <Button title='송금 카드 선택' onPress={() => { expnadSwitch(card) }} /> */}
-        <Divider size="small" />
-        <ProgressTab
-          title={`송금 카드 선택${fromAddress ? `: ${fromAddress}` : ''}`}
-          index={1}
-          onPress={() => {
-            expnadSwitch(card);
-            setFocus(1);
-          }}
-          state={focus === 1 ? 'focus' : fromAddress ? 'selected' : 'empty'}
-        />
-        {focus === 1 ? (
-          <ExpandableView width={card.width} height={card.height}>
-            <Text>11</Text>
-            {/* 카드 선택  */}
-            {/* from */}
-          </ExpandableView>
-        ) : null}
-        <Divider size="small" />
-        {/* <Button title='송금 대상 선택' onPress={() => { expnadSwitch(target) }} /> */}
-        <ProgressTab
-          title={`송금 대상 선택${to ? `: ${to}` : ''}`}
-          index={2}
-          onPress={() => {
+    const [card, setCard] = useState({
+        width: WIDTH,
+        height: HEIGHT,
+    });
+    const [target, setTarget] = useState({
+        width: FOLDED,
+        height: FOLDED,
+    });
+    const [tokenFlag, setTokenFlag] = useState({
+        width: FOLDED,
+        height: FOLDED,
+    });
+    const [amountFlag, setAmountFlag] = useState({
+        width: FOLDED,
+        height: FOLDED,
+    });
+    const [visible, setVisible] = useState(false);
+    const [nick, setNick] = useState('');
+    const [phone, setPhone] = useState('');
+    const [fromAddress, setFromAddress] = useState('');
+    useEffect(() => {
+        if (address) {
+            setFromAddress(address);
             expnadSwitch(target);
-            setFocus(2);
-          }}
-          state={focus === 2 ? 'focus' : to ? 'selected' : 'empty'}
-        />
-        {focus === 2 ? (
-          <ExpandableView width={target.width} height={target.height}>
-            <EasySendAndReceive address={to} setAddress={getTo} setType={getType} />
-          </ExpandableView>
-        ) : null}
-        <Divider size="small" />
-        {/* <Button title='토큰 종류 선택' onPress={() => { expnadSwitch(tokenFlag) }} /> */}
-        <ProgressTab
-          title={`토큰 종류 선택${token ? `: ${token}` : ''}`}
-          index={3}
-          onPress={() => {
-            expnadSwitch(tokenFlag);
-            setFocus(3);
-          }}
-          state={focus === 3 ? 'focus' : token ? 'selected' : 'empty'}
-        />
-        {focus === 3 ? (
-          <ExpandableView width={tokenFlag.width} height={tokenFlag.height}>
-            <TokenList selectedToken={token} setSelectedToken={setToken} />
-          </ExpandableView>
-        ) : null}
-        <Divider size="small" />
-        {/* <Button title='송금 수량 입력' onPress={() => { expnadSwitch(amountFlag) }} /> */}
-        <ProgressTab
-          title={`송금 수량 입력${amount ? `: ${amount}` : ''}`}
-          index={4}
-          onPress={() => {
-            expnadSwitch(amountFlag);
-            setFocus(4);
-          }}
-          state={focus === 4 ? 'focus' : amount ? 'selected' : 'empty'}
-        />
-        {focus === 4 ? (
-          <ExpandableView width={amountFlag.width} height={amountFlag.height}>
-            {/* 송금 수량 입력 */}
-            {/* amount */}
-            {/* <Input
-              label="송금 수량"
-              placeholder="수량 입력"
-              onChange={(e) => {
-                console.log(e);
-              }}
-            /> */}
-            <Input
-              placeholder="수량 입력"
-              keyboardType="numeric"
-              presetValue={amount}
-              setValue={setAmount}
-            />
-            <Won>{price}원</Won>
-          </ExpandableView>
-        ) : null}
-        <Divider size="small" />
-        <Button
-          title="송 금"
-          onPress={() => {
-            setVisible(true);
-          }}
-          disabled={fromAddress !== null && to !== null && token !== null && amount !== null}
-        />
-        {visible === true && (
-          <SendModal>
-            <Typography size="body1">송금내용을 확인할게요!</Typography>
-            <Typography size="body3">{nick !== '' && nick}</Typography>
-            <Typography size="body3">{phone !== '' && phone}</Typography>
-            <Typography size="body3">
-              {nick === '' && phone === '' && to.substring(0, 7) + '...'}
-            </Typography>
-            <Typography size="body3">님에게</Typography>
-            <Typography size="body3">{token} 토큰</Typography>
-            <Typography size="body3">{amount}개 보낼게요!</Typography>
-            <Button
-              title="네 맞아요!"
-              onPress={() => {
-                goConfirm();
-              }}
-            ></Button>
-            <TouchableWithoutFeedback
-              onPress={() => {
-                setVisible(false);
-              }}
-            >
-              <Close
-                source={{
-                  uri: 'https://user-images.githubusercontent.com/63468607/168971504-42ef3187-9a86-4266-abc5-9f3e5c8d0b4b.png',
-                }}
-              />
-            </TouchableWithoutFeedback>
-          </SendModal>
-        )}
-      </ScrollView>
-    </Container>
-  );
+        }
+    }, []);
+    const [to, setTo] = useState('');
+    const [price, setPrice] = useState(0);
+    const [token, setToken] = useState('');
+    const [amount, setAmount] = useState('');
+    //토큰
+    //수량
+
+    // @신지우
+    // 현재 선택된 단계
+    const [focus, setFocus] = useState(1);
+    const [selectedToken, setSelectedToken] = useState('');
+
+    const expnadSwitch = (val: ExpandProp) => {
+        const list = [card, target, tokenFlag, amountFlag];
+        const setList = [setCard, setTarget, setTokenFlag, setAmountFlag];
+        for (let i = 0; i < list.length; i++) {
+            if (list[i] === val) {
+                setList[i]({
+                    width: WIDTH,
+                    height: HEIGHT,
+                });
+            } else
+                setList[i]({
+                    width: FOLDED,
+                    height: FOLDED,
+                });
+        }
+    };
+
+    const goConfirm = () => {
+        //트랜잭션 id 들고 페이지 넘기기
+        //트랜잭션 요청 후 트랜잭션 id 가지고 다음 페이지로 이동
+        //@ts-ignore
+        navigate('Send', { screen: 'SendConfirm' });
+    };
+    const getType = (props: IType) => {
+        if (!props) return;
+        if (props.type === '전화번호') {
+            setPhone(props.value);
+            setNick('');
+        } else if (props.type === '닉네임') {
+            setNick(props.value);
+            setPhone('');
+        }
+        setTo('');
+    };
+    const getTo = (to: string) => {
+        setTo(to);
+        console.log(to);
+    };
+    const { navigate } = useNavigation();
+    return (
+        <ScrollView nestedScrollEnabled={true}>
+            <Container>
+                {/* <Button title='송금 카드 선택' onPress={() => { expnadSwitch(card) }} /> */}
+                <Divider size="small" />
+                <ProgressTab
+                    title={`송금 카드 선택${fromAddress ? `: ${fromAddress}` : ''}`}
+                    index={1}
+                    onPress={() => {
+                        expnadSwitch(card);
+                        setFocus(1);
+                    }}
+                    state={focus === 1 ? 'focus' : fromAddress ? 'selected' : 'empty'}
+                />
+                {focus === 1 ? (
+                    <ExpandableView width={card.width} height={card.height}>
+                        <Text>11</Text>
+                        {/* 카드 선택  */}
+                        {/* from */}
+                    </ExpandableView>
+                ) : null}
+                <Divider size="small" />
+                {/* <Button title='송금 대상 선택' onPress={() => { expnadSwitch(target) }} /> */}
+                <ProgressTab
+                    title={`송금 대상 선택${to ? `: ${to}` : ''}`}
+                    index={2}
+                    onPress={() => {
+                        expnadSwitch(target);
+                        setFocus(2);
+                    }}
+                    state={focus === 2 ? 'focus' : to ? 'selected' : 'empty'}
+                />
+                {focus === 2 ? (
+                    <ExpandableView width={target.width} height={target.height}>
+                        <EasySendAndReceive address={to} setAddress={getTo} setType={getType} />
+                    </ExpandableView>
+                ) : null}
+                <Divider size="small" />
+                {/* <Button title='토큰 종류 선택' onPress={() => { expnadSwitch(tokenFlag) }} /> */}
+                <ProgressTab
+                    title={`토큰 종류 선택${token ? `: ${token}` : ''}`}
+                    index={3}
+                    onPress={() => {
+                        expnadSwitch(tokenFlag);
+                        setFocus(3);
+                    }}
+                    state={focus === 3 ? 'focus' : token ? 'selected' : 'empty'}
+                />
+                {focus === 3 ? (
+                    <ExpandableView width={tokenFlag.width} height={tokenFlag.height}>
+                        <TokenList selectedToken={token} setSelectedToken={setToken} />
+                    </ExpandableView>
+                ) : null}
+                <Divider size="small" />
+                {/* <Button title='송금 수량 입력' onPress={() => { expnadSwitch(amountFlag) }} /> */}
+                <ProgressTab
+                    title={`송금 수량 입력${amount ? `: ${amount}` : ''}`}
+                    index={4}
+                    onPress={() => {
+                        expnadSwitch(amountFlag);
+                        setFocus(4);
+                    }}
+                    state={focus === 4 ? 'focus' : amount ? 'selected' : 'empty'}
+                />
+                {focus === 4 ? (
+                    <ExpandableView width={amountFlag.width} height={amountFlag.height}>
+                        {/* 송금 수량 입력 */}
+                        {/* amount */}
+                        <Input
+                            placeholder="수량 입력"
+                            keyboardType="numeric"
+                            presetValue={amount}
+                            setValue={setAmount}
+                        />
+                        <Won>{price}원</Won>
+                    </ExpandableView>
+                ) : null}
+                <Divider size="small" />
+                {/* <Button title="클릭" onPress={() => { console.log(fromAddress, to, token, amount) }} /> */}
+                <Button
+                    title="송 금"
+                    onPress={() => {
+                        setVisible(true);
+                    }}
+                    disabled={fromAddress === "" || to === "" || token === "" || amount === ""}
+                />
+                {/* 
+                    Todo
+                    가스비 선택
+                    
+                    TextButton -> 직접 입력 할레요!
+
+                    
+                */}
+                {visible === true && (
+                    <SendModal>
+                        <Typography size="body1">송금내용을 확인할게요!</Typography>
+                        <Typography size="body3">{nick !== '' && nick}</Typography>
+                        <Typography size="body3">{phone !== '' && phone}</Typography>
+                        <Typography size="body3">
+                            {nick === '' && phone === '' && to.substring(0, 7) + '...'}
+                        </Typography>
+                        <Typography size="body3">님에게</Typography>
+                        <Typography size="body3">{token} 토큰</Typography>
+                        <Typography size="body3">{amount}개 보낼게요!</Typography>
+                        <Button
+                            title="네 맞아요!"
+                            onPress={() => {
+                                goConfirm();
+                            }}
+                        ></Button>
+                        <TouchableWithoutFeedback
+                            onPress={() => {
+                                setVisible(false);
+                            }}
+                        >
+                            <Close
+                                source={{
+                                    uri: 'https://user-images.githubusercontent.com/63468607/168971504-42ef3187-9a86-4266-abc5-9f3e5c8d0b4b.png',
+                                }}
+                            />
+                        </TouchableWithoutFeedback>
+                    </SendModal>
+                )}
+            </Container>
+        </ScrollView>
+    );
 };
 
 export default Send;
--- Frontend/app/screens/Send/SendConfirm.tsx
+++ Frontend/app/screens/Send/SendConfirm.tsx
@@ -1,16 +1,37 @@
 import Button from "@components/common/Button"
 import TextButton from "@components/common/TextButton/TextButton"
+import { useNavigation } from "@react-navigation/native";
 import { useState } from "react"
-import { Text, View } from "react-native"
+import { Image, Text, View } from "react-native"
+import styled from "styled-components/native";
 
-export const SendConfirm = () => {
+interface SendConfirmProps {
+    transaction: string;
+}
+
+
+export const SendConfirm: React.FC<SendConfirmProps> = (transaction) => {
+    const { navigate } = useNavigation();
     const [cancle, setCancle] = useState(false);
+    const goMain = () => {
+        console.log("찍힘");
+        navigate('Tabs', { screen: '지갑' });
+    };
     return (
         <View>
             <Text>송금 확인</Text>
             <TextButton title="송금 요청을 취소하고 싶어요!" onPress={() => { setCancle(true) }} />
             <Text style={{ color: 'red' }}>(요청 취소 시 별도의 수수료가 들 수 있어요!)</Text>
-            <Button title="확 인" onPress={() => { }} />
+            <Check source={{ uri: 'https://user-images.githubusercontent.com/63468607/168994200-5966cd13-660e-42b3-aad8-be3e77fc4eef.png' }} resizeMode="contain" />
+            <Button title="확 인" onPress={() => { goMain() }} />
         </View>
     )
 }
+
+const Check = styled.Image`
+    width: 80%;
+    height: 80%;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+`;
