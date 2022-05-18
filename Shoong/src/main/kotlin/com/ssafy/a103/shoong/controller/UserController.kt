10067d4828163f9816109b5aa180104eaf2fcf8f
prkg7341 <prkg7341@gmail.com>
2022-05-18 13:40:02 +0900

    [S06P31A103-87] :bug: Fix: Fix jwt 401 problem

--- Shoong/src/main/kotlin/com/ssafy/a103/shoong/controller/UserController.kt
+++ Shoong/src/main/kotlin/com/ssafy/a103/shoong/controller/UserController.kt
@@ -19,11 +19,6 @@ import javax.servlet.http.HttpServletResponse
 @RestController
 private class UserController(val userService: UserService) {
 
-    // TODO JWT 토큰 관련 코드 추가
-
-    // TODO 비밀번호 암호화 함수 추가
-
-    // TODO 필요한대로 수정
     @Operation(summary = "get all users", description = "get all users")
     @ApiResponses(value=[
         ApiResponse(responseCode = "200", description = "OK !!"),
