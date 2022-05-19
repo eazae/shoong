facd251a23388d83218fb2149acb484faf237b11
YoonBaek <royal729@naver.com>
2022-05-20 00:07:44 +0900

    [S06P31A103-66] ✨ Feat: 카드 삭제, 리프레시 구현

--- Frontend/app/services/api/card/cardAPI.ts
+++ Frontend/app/services/api/card/cardAPI.ts
@@ -1,3 +1,4 @@
+import { CardApiProps } from '@components/item/Card/Card.props';
 import instance from '../axios';
 
 const COMMON = '/user';
@@ -5,4 +6,9 @@ const COMMON = '/user';
 export const getCardsFromSHOONG = async () => {
   const response = await instance.post(COMMON + '/loadCard');
   return response.data;
+};
+
+export const deleteCardFromSHOONG = async (data: CardApiProps) => {
+  const response = await instance.post(COMMON + '/deleteCard', data);
+  return response;
 };
--- Frontend/index.js
+++ Frontend/index.js
@@ -69,3 +69,4 @@ PushNotification.configure({
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+console.disableYellowBox = true;
